6f7146f5f02aa5933195d34cde211106e2ab4319
Marcos <velozmarkdrea@gmail.com>
2015-09-10 19:01:06 +0200

    new tests running

--- src/Pomodoro/Pomodoro.php
+++ src/Pomodoro/Pomodoro.php
@@ -4,17 +4,46 @@ namespace MarcosSegovia\Pomodoro;
 
 class Pomodoro
 {
-    private $duration;
+    const DEFAULT_DURATION = 25;
 
-    public function __construct($duration)
+    private $duration;
+    private $status;
+
+
+    public function __construct($duration = self::DEFAULT_DURATION)
     {
-        $this->duration = $duration;
+        if ($duration > self::DEFAULT_DURATION) {
+            $this->duration = self::DEFAULT_DURATION;
+        } else {
+            $this->duration = $duration;
+        }
+
+        $this->status = 'stop';
     }
+
+    public function play()
+    {
+        try {
+            if (strcmp($this->getStatus(), 'stop') != 0) {
+                throw new \Exception('Pomodoro is not stop !');
+            } else {
+                $this->status = 'play';
+            }
+        } catch (\Exception $e) {
+
+            echo $e->getMessage();
+        }
+    }
+
 
     public function getDuration()
     {
         return $this->duration;
     }
 
+    public function getStatus()
+    {
+        return $this->status;
+    }
 
 }
--- tests/PomodoroTest.php
+++ tests/PomodoroTest.php
@@ -6,16 +6,33 @@ use MarcosSegovia\Pomodoro\Pomodoro;
 
 class PomodoroTest extends \PHPUnit_Framework_TestCase
 {
-    private $pomodoro;
-
-    public function setUp()
-    {
-        $this->pomodoro = new Pomodoro(25);
-    }
 
     public function testHasDurationUnder25()
     {
-        $result = $this->pomodoro->getDuration() <= 25 ? true : false;
+        $pomodoro = new Pomodoro(15);
+        $result = $pomodoro->getDuration() <= 25 ? true : false;
+        $this->assertEquals(true, $result);
+    }
+
+    public function testHasByDefault25Minutes()
+    {
+        $pomodoro = new Pomodoro();
+        $result = $pomodoro->getDuration() <= 25 ? true : false;
+        $this->assertEquals(true, $result);
+    }
+
+    public function testPomodoroIsStopWhenIsCreated()
+    {
+        $pomodoro = new Pomodoro();
+        $result = strcmp($pomodoro->getStatus(), 'stop') == 0 ? true : false;
+        $this->assertEquals(true, $result);
+    }
+
+    public function testPomodoroIsPlayed()
+    {
+        $pomodoro = new Pomodoro();
+        $pomodoro->play();
+        $result = strcmp($pomodoro->getStatus(), 'play') == 0 ? true : false;
         $this->assertEquals(true, $result);
     }
 
